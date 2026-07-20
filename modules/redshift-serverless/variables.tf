3e9489b6157d52868d881e776dbe374120bb9c0b
Jim Williams <jwilliams@truemark.io>
2026-07-20 13:22:09 -0600

    Modifying this module to allow for boolean valuables to be set for publicly_accessible and enhanced_vpc_routing

--- modules/redshift-serverless/variables.tf
+++ modules/redshift-serverless/variables.tf
@@ -43,3 +43,15 @@ variable "base_capacity" {
   default     = 32
   description = "Redshift Serverless base capacity in RPU (e.g., 8, 16, 32, etc.)"
 }
+
+variable "enhanced_vpc_routing" {
+  type        = bool
+  default     = true
+  description = "Specifies whether to turn on enhanced virtual private cloud (VPC) routing, which forces Amazon Redshift Serverless to route traffic through your VPC instead of over the internet.)"
+}
+
+variable "publicly_accessible" {
+  type        = bool
+  default     = false
+  description = "Can workgroup can be accessed from a public network.)"
+}
